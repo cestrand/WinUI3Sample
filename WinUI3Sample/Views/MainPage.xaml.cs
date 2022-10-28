a01dc392fb0ae6d5a093e851515ab1a33c2fca9b
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-10-29 00:52:11 +0200

    Main page with app purpose

--- WinUI3Sample/Views/MainPage.xaml.cs
+++ WinUI3Sample/Views/MainPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.UI.Xaml.Controls;
-
+﻿using System.Diagnostics;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
 using WinUI3Sample.ViewModels;
 
 namespace WinUI3Sample.Views;
@@ -15,5 +16,12 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+    }
+
+    private async void OpenUri_WinUI3_Documentation(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        // Open WinUI3 documentation
+        Uri uri = new Uri("https://learn.microsoft.com/en-us/windows/apps/winui/");
+        Windows.System.Launcher.LaunchUriAsync(uri);
     }
 }
